ecc5aae6c9ba9243af2d1e88b591583a9be3c68e
gcronax <guillecampher15@gmail.com>
2024-12-18 18:52:41 +0100

    wqeqwe

--- src/ej4_1.java
+++ src/ej4_1.java
@@ -1,3 +1,16 @@
-public class ej4_1 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
+public class ej4_1 {
+    public static void main(String[] args) {
+        String prueba="qwertyuiioopdddfvv";
+        char[] aux = prueba.toCharArray();
+        ArrayList retorno=new ArrayList();
+        for (char c: aux){
+            retorno.add(c);
+        }
+        
+        System.out.println(Arrays.toString(retorno.stream().distinct().toArray()));
+    }
 }
