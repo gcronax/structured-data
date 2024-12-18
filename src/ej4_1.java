bf04fab5341b8f8502d1e30932a2b17adfbfeaf7
gcronax <guillecampher15@gmail.com>
2024-12-18 19:05:53 +0100

    wqeqwe

--- src/ej4_1.java
+++ src/ej4_1.java
@@ -5,12 +5,25 @@ import java.util.Objects;
 public class ej4_1 {
     public static void main(String[] args) {
         String prueba="qwertyuiioopdddfvv";
-        char[] aux = prueba.toCharArray();
-        ArrayList retorno=new ArrayList();
-        for (char c: aux){
-            retorno.add(c);
+        String retorno="";
+
+        for (int i = 0; i < prueba.length(); i++) {
+            int n=0;
+            boolean unico=true;
+            for (int j = 0; j < prueba.length(); j++) {
+                    if (prueba.charAt(i)==prueba.charAt(j)){
+                        n++;
+                    }
+                    if (n>1&&prueba.charAt(i)==prueba.charAt(j)){
+                        unico=false;
+                    }
+
+
+            }
+            if (unico){
+                retorno+=prueba.charAt(i);
+            }
         }
-        
-        System.out.println(Arrays.toString(retorno.stream().distinct().toArray()));
+        System.out.println(retorno);
     }
 }
